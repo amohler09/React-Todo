265083be2437a9386dd54091556cdc60264eb1aa
Amber Mohler <ambermohler@iMac.local>
2020-01-14 14:48:34 -0500

    added todo component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm'
+import TodoList from './components/TodoComponents/TodoList'
+
+const data = [
+  {
+    task: 'Unpack Clothes',
+    id: 123468,
+    completed: false
+  },
+  {
+    task: 'Organize Kitchen',
+    id: 685643,
+    completed: false
+  },
+  {
+    task: 'Do Dishes',
+    id: 365954,
+    completed: false
+  },
+  {
+    task: 'Wash Clothes',
+    id: 497358,
+    completed: false
+  },
+  {
+    task: 'Give the Dog a Bath',
+    id: 854965,
+    completed: false
+  },
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super ();
+
+    this.state = {
+      todoList: data
+    }
+  }
+  
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='App'>
+        <div className='top'>
+        <h1>To Do List</h1>
+        <TodoForm />
+        <TodoList todo={this.state.todoList} />
+        </div>
+       
       </div>
     );
   }
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Todo = props => {
+    return (
+        <div className='todo'>
+            <p>{props.item.task}</p>
+        </div>
+    )
+}
